c0210e9dc6ccd254228fd6991c6495fdf933af43
thierabsy <thierabsy@yahoo.fr>
2018-10-22 02:21:21 +0100

    Changed rootPath from localhost to heroku

--- src/utils/path.js
+++ src/utils/path.js
@@ -1,3 +1,4 @@
 export const path = "http://localhost:3000";
 
-export const rootPath = "http://localhost:5001/";
+export const rootPath = "https://murmuring-brook-27050.herokuapp.com/";
+// export const rootPath = "http://localhost:5001/";
